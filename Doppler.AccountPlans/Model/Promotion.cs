eb947733302a55829e566d7d46426cec52c70a80
nreal <nreal@makignsense.com>
2021-12-15 16:34:45 -0300

    feat: add promocodeId in promotion

--- Doppler.AccountPlans/Model/Promotion.cs
+++ Doppler.AccountPlans/Model/Promotion.cs
@@ -2,6 +2,7 @@ namespace Doppler.AccountPlans.Model
 {
     public class Promotion
     {
+        public int IdPromotion { get; set; }
         public int? ExtraCredits { get; set; }
         public int? DiscountPercentage { get; set; }
     }
