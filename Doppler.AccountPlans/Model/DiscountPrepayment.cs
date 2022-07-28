ac5002264fa46e044d0dce9ebd5f1aea4e5a2c0d
jhoffman-ms <jmh248@gmail.com>
2022-07-28 15:28:18 -0300

    feat: integrate update between contacts plans

--- Doppler.AccountPlans/Model/DiscountPrepayment.cs
+++ Doppler.AccountPlans/Model/DiscountPrepayment.cs
@@ -3,6 +3,8 @@ namespace Doppler.AccountPlans.Model
     public class DiscountPrepayment
     {
         public decimal Amount { get; set; }
+        public decimal NextAmount { get; set; }
         public decimal DiscountPercentage { get; set; }
+        public int MonthsToPay { get; set; }
     }
 }
