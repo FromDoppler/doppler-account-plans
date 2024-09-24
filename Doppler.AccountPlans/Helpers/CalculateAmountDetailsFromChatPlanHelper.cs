5b751091dfdd2177801e036f1e5c456e7154fdf7
jhoffman-ms <jmh248@gmail.com>
2024-09-24 08:30:27 -0300

    fix: fix issue when calculate next invoice date

--- Doppler.AccountPlans/Helpers/CalculateAmountDetailsFromChatPlanHelper.cs
+++ Doppler.AccountPlans/Helpers/CalculateAmountDetailsFromChatPlanHelper.cs
@@ -110,7 +110,7 @@ namespace Doppler.AccountPlans.Helpers
             result.NextMonthTotal = ((newPlan.ChatPlanFee ?? 0) * newDiscount.MonthPlan) - result.DiscountPlanFeeAdmin.NextAmount - result.DiscountPrepayment.NextAmount;
             result.MajorThat21st = now.Day > 21;
 
-            var nexMonnthInvoiceDate = now.AddMonths(differenceBetweenMonthPlans);
+            var nexMonnthInvoiceDate = !isMonthPlan ? now.AddMonths(differenceBetweenMonthPlans) : now.AddMonths(1);
             result.NextMonthDate = new DateTime(nexMonnthInvoiceDate.Year, nexMonnthInvoiceDate.Month, 1);
 
             return result;
