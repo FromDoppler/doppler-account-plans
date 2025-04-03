3e973ee8b81a1348d47c6a33074431e8706f3fa2
homecavs <homecavs@gmail.com>
2025-04-03 11:41:48 -0300

    chore: fix format

--- Doppler.AccountPlans/Infrastructure/AccountPlansRepository.cs
+++ Doppler.AccountPlans/Infrastructure/AccountPlansRepository.cs
@@ -580,8 +580,8 @@ SELECT [IdPushNotificationPlan] AS PlanId
         ,[Fee]
         ,[Additional]
         ,5 AS PlanType
- FROM [dbo].[PushNotificationPlan]
- WHERE [Active] = 1 AND [Fee] > 0");
+FROM [dbo].[PushNotificationPlan]
+WHERE [Active] = 1 AND [Fee] > 0");
 
             return result;
         }
@@ -596,8 +596,8 @@ SELECT [IdPushNotificationPlan] AS PlanId
         ,[Fee]
         ,[Additional]
         ,5 AS PlanType
- FROM [dbo].[PushNotificationPlan]
- WHERE [Custom] = 1 AND [Fee] > 0");
+FROM [dbo].[PushNotificationPlan]
+WHERE [Custom] = 1 AND [Fee] > 0");
 
             return result;
         }
