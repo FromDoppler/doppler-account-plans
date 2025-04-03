49f08ac358590f403fd6171e41d8f2431b19b0bf
homecavs <homecavs@gmail.com>
2025-04-03 11:30:41 -0300

    feat: add endpoint for get plans of any addon

--- Doppler.AccountPlans/Model/PushNotificationPlanInformation.cs
+++ Doppler.AccountPlans/Model/PushNotificationPlanInformation.cs
@@ -5,7 +5,6 @@ namespace Doppler.AccountPlans.Model
         public string Description { get; set; }
         public int Quantity { get; set; }
         public decimal Fee { get; set; }
-        public int? FreeDays { get; set; }
         public decimal Additional { get; set; }
     }
 }
